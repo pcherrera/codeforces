56b601dc835c141afb21fcbc316502f5552aa760
pcherrera <58631255+pcherrera@users.noreply.github.com>
2022-01-18 23:13:39 -0300

    Add files via upload

---
+++ Awatermelon.cpp
@@ -0,0 +1,18 @@
+//A. Watermelon
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    // Introduce the watermelon weight
+    int w;
+    cin >> w ;
+    if (w % 2 == 0){
+        if (w == 2){
+            cout << "NO";
+        } else{
+        cout << "YES";
+        }
+    } else{
+        cout << "NO";
+    }
+    return 0;
+}
