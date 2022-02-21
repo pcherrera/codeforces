e9490e8107dc0bc52c5cae0a12cde5190b9f9556
pcherrera <58631255+pcherrera@users.noreply.github.com>
2022-02-21 02:08:34 -0300

    Add files via upload

---
+++ ADominoPiling.cpp
@@ -0,0 +1,10 @@
+// A Domino Piling
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m;
+    cin >> n >> m;
+    int number_of_tiles = n*m/2;
+    cout << number_of_tiles;
+    return 0;
+}
